b4d78e24eca0165e4ebb2e7572bd9d4d4aca8cef
KhaledCS1 <Khaled.c003+1@gmail.com>
2025-05-02 03:18:46 +0300

    Create Project_Zero.py

---
+++ Project_Zero.py
@@ -0,0 +1,9 @@
+# Python101 Project_Zero-Satr Academy
+# build an App for Driving licenses
+
+my_age = int(input('What is your age?'))
+if my_age == 18:
+    print('Your Age is', my_age, 'Eligible for Driving License')
+
+elif my_age <= 17:
+    print('Your Age is', my_age, 'Not Eligible for Driving License')
